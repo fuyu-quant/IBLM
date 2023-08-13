309adb10923b94838478f6700c1e76b66a39a2eb
toma tanaka <ulti4929@gmail.com>
2023-08-13 23:39:17 +0900

    update

--- src/iblm/iblmodel/iblmodel.py
+++ src/iblm/iblmodel/iblmodel.py
@@ -1,39 +1,32 @@
 from langchain.callbacks import get_openai_callback
-import re
-
 import numpy as np
-
+import pandas as pd
+from importlib import resources
 import warnings
+
 warnings.filterwarnings('ignore')
 
 
-class IBLMRegressor():
+class IBLModel():
     def __init__(
         self, 
-        llm_model, 
-        params
+        llm_model,
+        params,
+        mode,
         ):
         self.llm_model = llm_model
         self.columns_name = params['columns_name']
+        self.mode = mode
         self.model_code = None
 
 
-    def fit(self, x, y, model_name, file_path=None):
-        print("> Start of model creating.")
+    def fit(self, x, y, prompt = None, model_name = None, file_path = None):
         df = x.copy()
-
         df['target'] = y
 
-        # Determine whether binary or multivalued classification is used
-        if len(df['target'].unique()) == 2:
-            task_type = 'binary classification'
-            output_code = 'y = 1 / (1 + np.exp(-y))'
-        else:
-            task_type = 'multi-class classification'
 
         # Obtaining data types
         data_type = ', '.join(df.dtypes.astype(str))
-
 
 
         # Create a string dataset
@@ -56,63 +49,38 @@ class IBLMRegressor():
             col_option = 'df.columns = range(df.shape[1])'
 
 
-
-        create_prompt = """
-        Please create your code in compliance with all of the following conditions. Output should be code only. Do not enclose the output in ``python ``` or the like.
-        ・Analyze the large amount of data below and create a {task_type_} code to accurately predict "target".
-        ------------------
-        {dataset_str_}
-        ------------------
-        ・Each data type is as follows. If necessary, you can change the data type.
-        ・Create code that can make predictions about new data based on logic from large amounts of input data without using machine learning models.
-        ・If input is available, the column names below should also be used to help make decisions when creating the predictive model. Column Name:{col_name_}
-        ・Create a code like the following. Do not change the input or output format.
-        ・If {col_option_} is not blank, add it after 'df = x.copy()'.
-        ・You do not need to provide examples.
-        ------------------
-        import numpy as np
-
-        def predict(x):
-            df = x.copy()
-
-            output = []
-            for index, row in df.iterrows():
+        # create prompt
+        if prompt == None:
+            if self.mode == 'regression':
+                with resources.open_text('iblm.iblmodel.prompt', 'regression.txt') as file:
+                    prompt = file.read()
+            elif self.mode == 'classification':
+                with resources.open_text('iblm.iblmodel.prompt', 'classification.txt') as file:
+                    prompt = file.read()
 
 
-                # Feature creation and data preprocessing
-
-
-                {output_code_}
-                output.append(y)
-
-            output = np.array(output)
-                
-            return output
-        """.format(
-            task_type_ = task_type,
+        create_prompt = prompt.format(
             dataset_str_ = dataset_str,
-            model_name_ = model_name,
+            data_type_ = data_type,
             col_name_ = col_name,
-            col_option_ = col_option,
-            output_code_ = output_code
+            col_option_ = col_option
             )
 
-        #print(create_prompt)
 
         with get_openai_callback() as cb:
             model_code = self.llm_model(create_prompt)
             print(cb)
-
 
         # Save to File
         if file_path != None:
             with open(file_path + f'{model_name}.py', mode='w') as file:
                 file.write(model_code)
 
-
         self.model_code = model_code
 
         return model_code
+
+
 
     def predict(self, x):
         if self.model_code is None:
@@ -120,14 +88,9 @@ class IBLMRegressor():
 
         code = self.model_code
 
-        # = re.search(r'def (\w+)', function_string).group(1)
-        #code = self.model_code + '\n'# + f'model = model({x})'
         exec(code, globals())
 
-        #model = namespace["code"]
-        
         y = predict(x)
-
         return y
 
 
